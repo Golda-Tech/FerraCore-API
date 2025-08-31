49441c62ac2a683a92af9d0b05d007d7a7e87936
Jonathan Ato Markin <jona10711@gmail.com>
2025-08-31 16:35:11 +0200

    api gateway module created and added

---
+++ api-gateway/src/test/java/com/goldatech/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldatech.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
