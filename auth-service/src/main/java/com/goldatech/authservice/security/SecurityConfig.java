26eac255a8c48c9465fa7d163de58574a2926e61
Jonathan Ato Markin <jona10711@gmail.com>
2025-12-19 15:13:33 +0100

    Actuators

--- auth-service/src/main/java/com/goldatech/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/goldatech/authservice/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
             "/api/v1/health/**",
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            "/swagger-ui.html"
+            "/swagger-ui.html",
+            "/actuator/**"
     };
 
     @Bean
