42780f8ba0681334edd964e36c833138b7c7a4a0
jonas.korankye <jonas.korankye@softwaregroup.com>
2026-01-21 11:32:17 +0000

    payment ledger repo fix

--- payment-service/src/main/java/com/goldatech/paymentservice/domain/repository/PaymentLedgerRepository.java
+++ payment-service/src/main/java/com/goldatech/paymentservice/domain/repository/PaymentLedgerRepository.java
@@ -3,6 +3,9 @@ package com.goldatech.paymentservice.domain.repository;
 import com.goldatech.paymentservice.domain.model.PaymentLedger;
 import com.goldatech.paymentservice.domain.model.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -10,5 +13,8 @@ import java.util.UUID;
 @Repository
 public interface PaymentLedgerRepository extends JpaRepository<PaymentLedger, UUID> {
 
-    void updateStatusByTransactionId(TransactionStatus status, String transactionRef);
+    @Modifying
+    @Query("update PaymentLedger p set p.status = :status where p.transactionId = :transactionId")
+    void updateStatusByTransactionId(@Param("status") TransactionStatus status,
+                                     @Param("transactionId") String transactionId);
 }
